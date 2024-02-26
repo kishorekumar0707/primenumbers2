161c5b43688feb5a720f652e2df4dce6a7625d0b
Kishore Kumar N <kishorekumar07070@gmail.com>
2024-02-26 16:31:46 +0530

    Add files via upload

---
+++ primenumbers.js
@@ -0,0 +1,9 @@
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let primes = arr.filter(num => {
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) return false;
+    }
+    return num > 1;
+  });
+  console.log(primes);
